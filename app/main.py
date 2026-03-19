2dfe3d169ab27c427436f9ce7fda8bbc93f7c47f
Fran0808 <fortegaquipse@gmail.com>
2026-03-19 10:14:51 -0500

    feat: import subprocess

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 
 def main():
     if len(sys.argv) < 2:
@@ -7,6 +8,13 @@ def main():
 
     target = sys.argv[1]
     print(f"Target: {target}")
+
+    result = subprocess.run(
+        ["nmap", "--version"],
+        capture_output=True,
+        text=True
+    )
+    print(result.stdout)
 
 if __name__ == "__main__":
     main()
